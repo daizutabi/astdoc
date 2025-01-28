c3c1fc190a68d6b3d5b1ba32683519a86477ea46
Daizu <daizutabi@gmail.com>
2025-01-28 23:36:23 +0000

    parser/test_merge

--- tests/parser/test_merge.py
+++ tests/parser/test_merge.py
@@ -107,17 +107,17 @@ def test_merge_attribute_class():
 def test_merge_attribute_enum():
     from astdoc.parser import merge_attributes
 
-    cls = get_object("astdoc.page.PageKind")
+    cls = get_object("inspect._ParameterKind")
     assert isinstance(cls, Class)
     attrs = [x for _, x in cls.get_children(Type)]
-    assert len(attrs) >= 3  # 3 for python 3.10, 5 for python >=3.11
+    assert len(attrs) >= 5
     sections = cls.doc.sections
     names = ["name", "value"]
     merge_attributes(sections, attrs, ignore_names=names, ignore_empty=False)
     section = find_item_by_name(sections, "Attributes")
     assert isinstance(section, Section)
     items = section.items
-    assert len(items) == 3
+    assert len(items) == 5
 
 
 def test_merge_attribute_private():
